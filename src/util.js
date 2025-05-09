4ce746287c3b868216e2dc333e92584c063f47bf
ny-aina-solofo <nyainarandrianarisolo@gmail.com>
2025-05-09 13:03:36 +0300

    init : initialisation avec npm et ajout d'un package json

--- src/util.js
+++ src/util.js
@@ -1,18 +1,6 @@
 const fs = require('node:fs');
 const DATA_FILE_NAME = "data.json";
-// let data = [
-//     {
-//         id_task : 18,
-//         libelle:"read todo",
-//         status: false,
-//         createdAt:"2025-04-25",
-//         updatedAt:"2025-05-01"
-//     }
-// ];
 
-// if (!fs.existsSync(DATA_FILE_NAME)) {
-//     fs.writeFileSync(DATA_FILE_NAME, JSON.stringify([]));
-// }
 const data = JSON.parse(fs.readFileSync(DATA_FILE_NAME, 'utf8'));
 
 const saveTodo = (updatedTask) => {
