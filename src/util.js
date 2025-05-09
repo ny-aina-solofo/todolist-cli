551ce21234f306b1f0b6c62ebd6a5567be2ccbaa
ny-aina-solofo <nyainarandrianarisolo@gmail.com>
2025-05-09 13:13:59 +0300

    feat : ajout d'un fonction de création automatique d'un fichier data.json si celui-ci n'existe pas

--- src/util.js
+++ src/util.js
@@ -1,6 +1,9 @@
 const fs = require('node:fs');
 const DATA_FILE_NAME = "data.json";
 
+if (!fs.existsSync(DATA_FILE_NAME)) {
+    fs.writeFileSync(DATA_FILE_NAME, JSON.stringify([]));
+}
 const data = JSON.parse(fs.readFileSync(DATA_FILE_NAME, 'utf8'));
 
 const saveTodo = (updatedTask) => {
